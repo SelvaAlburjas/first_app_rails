8bd998ab33fc8a45c7d68c9a05b54bca1b753407
SelvaAlburjas <selvadianett@gmail.com>
2022-06-20 19:45:11 -0400

    Tercer commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
   root 'pages#one'
+  get 'pages/three'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
